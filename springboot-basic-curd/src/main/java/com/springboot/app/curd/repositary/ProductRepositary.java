857ef0a3fb5c1fe60e194b206a0fe3185fbad954
RaniChavan13 <ranichavan109@gmail.com>
2023-04-14 20:35:02 +0530

    push

---
+++ springboot-basic-curd/src/main/java/com/springboot/app/curd/repositary/ProductRepositary.java
@@ -0,0 +1,14 @@
+package com.springboot.app.curd.repositary;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.app.curd.entity.Product;
+
+public interface ProductRepositary extends JpaRepository<Product, Integer>{
+
+
+	Optional<Product> findByName(String name);
+
+}
